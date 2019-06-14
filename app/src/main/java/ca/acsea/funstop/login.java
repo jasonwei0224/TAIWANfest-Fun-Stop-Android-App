cf2e67588397b80f9d3a6cd875be0c7567d370c6
jasonwei0224 <45651276+jasonwei0224@users.noreply.github.com>
2019-06-14 11:12:22 -0700

    updated camera for toronto add permission for camera add gms service and vision service

--- app/src/main/java/ca/acsea/funstop/login.java
+++ app/src/main/java/ca/acsea/funstop/login.java
@@ -1,18 +1,68 @@
 package ca.acsea.funstop;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class login extends AppCompatActivity {
 
-
-
+    private CheckBox agreeToReceiveEmail;
+    private CheckBox agreeToProgramNotification ;
+    private CheckBox agreeToJoinPoolOfPrize;
+    private EditText editTextEmail;
+    private Spinner cityChoice;
+    private String city;
+    private boolean agreedToReceiveEmailIsChecked;
+    private boolean agreedToProgramNotification;
+    private boolean agreedToJoinBigPrize;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        agreeToReceiveEmail = findViewById(R.id.agreeToReceiveEmail);
+        agreeToJoinPoolOfPrize = findViewById(R.id.agreeToJoinPoolOfPrize);
+        agreeToProgramNotification = findViewById(R.id.agreeToProgramNotification);
+
+        agreedToReceiveEmailIsChecked = agreeToReceiveEmail.isChecked();
+        /*if(agreedToReceiveEmailIsChecked){
+            Toast.makeText(this, "checked", Toast.LENGTH_LONG).show();
+        }*/
+        agreeToReceiveEmail.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                    agreedToReceiveEmailIsChecked = isChecked;
+                    //Change the picture
+                    //Toast.makeText(login.this, "checked", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
+
+    private void createUser(){
+        agreedToJoinBigPrize = agreeToJoinPoolOfPrize.isChecked();
+        agreedToProgramNotification = agreeToProgramNotification.isChecked();
+        city = String.valueOf(cityChoice.getSelectedItem());
+        final String email = editTextEmail.getText().toString();
+        if(!agreedToReceiveEmailIsChecked){
+            agreeToReceiveEmail.setError("Must be agree to use our app");
+            agreeToReceiveEmail.requestFocus();
+            return;
+        }
+        if (email.isEmpty()) {
+            editTextEmail.setError("Email required");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+    }
+
 
 }
--- app/src/main/java/ca/acsea/funstop/torCamera.java
+++ app/src/main/java/ca/acsea/funstop/torCamera.java
@@ -1,13 +1,131 @@
 package ca.acsea.funstop;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.SparseArray;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.TextView;
+import com.google.android.gms.vision.CameraSource;
+
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
 
 public class torCamera extends AppCompatActivity {
+    SurfaceView cameraPreview;
+    TextView txtResult;
+    BarcodeDetector barcodeDetector;
+    CameraSource cameraSource;
+    public static final int RequestCameraPermissionID = 1001;
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permission, int[] grantResults){
+        switch (requestCode){
+            case RequestCameraPermissionID:{
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    if(ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+                        ActivityCompat.requestPermissions(torCamera.this,
+                                new String[]{Manifest.permission.CAMERA}, RequestCameraPermissionID);
+                        return;
+                    }
+                    try {
+                        cameraSource.start(cameraPreview.getHolder());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    }
+                }
+            }
+        }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tor_camera);
+        Intent intent = getIntent();
+        cameraPreview = (SurfaceView)findViewById(R.id.TorCameraView);
+        txtResult = (TextView)findViewById(R.id.txtResult);
+        barcodeDetector = new BarcodeDetector.Builder(this)
+                .setBarcodeFormats(Barcode.QR_CODE)
+                .build();
+        cameraSource = new CameraSource
+                .Builder(this, barcodeDetector)
+                .setRequestedPreviewSize(640,480)
+                .build();
+
+        cameraPreview.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder surfaceHolder) {
+                if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(torCamera.this,
+                            new String[]{Manifest.permission.CAMERA}, RequestCameraPermissionID);
+                    return;
+                }
+                try {
+                    cameraSource.start(cameraPreview.getHolder());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+            @Override
+            public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2){
+
+            }
+            @Override
+            public void surfaceDestroyed(SurfaceHolder surfaceHolder){
+                cameraSource.stop();
+            }
+
+        });
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>(){
+            @Override
+            public void release() {
+            }
+            @Override
+            public void receiveDetections(Detector.Detections < Barcode > detections) {
+                final SparseArray<Barcode> qrcodes = detections.getDetectedItems();
+                if (qrcodes.size() != 0) {
+                    txtResult.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            txtResult.setText(qrcodes.valueAt(0).displayValue);
+                            Intent intent = new Intent(torCamera.this, TorFunStop.class);
+                            intent.putExtra("barcode", qrcodes.valueAt(0));
+                            startActivity(intent);
+                        }
+                    });
+                    //finish();
+                 /*txtResult.post(new Runnable() {
+                    @Override
+                    public void run() {
+                    Vibrator vibrator = (Vibrator) getApplicationContext().getSystemService(Context.VIBRATOR_SERVICE);
+                        //vibrator.vibrate(VibrationEffect.DEFAULT_AMPLITUDE);
+
+                        try{
+                            stationID = Integer.parseInt(qrcodes.valueAt(0).rawValue);
+                            txtResult.setText(stationID);
+
+
+                        }catch(Exception e){
+                            stationID = 0;
+                            txtResult.setText(qrcodes.valueAt(0).displayValue);
+                        }
+
+
+                            }
+                        });*/
+                }
+            }
+        });
     }
 }
+
