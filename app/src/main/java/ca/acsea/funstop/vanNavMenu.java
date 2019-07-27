92e066a9bd30f688f0fff1c187f69baf38eb9809
jasonwei0224 <45651276+jasonwei0224@users.noreply.github.com>
2019-07-27 14:04:50 -0700

    update vancouver funstop layout

--- app/src/main/java/ca/acsea/funstop/vanNavMenu.java
+++ app/src/main/java/ca/acsea/funstop/vanNavMenu.java
@@ -3,14 +3,24 @@ package ca.acsea.funstop;
 import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-
+import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.AdRequest;
 public class vanNavMenu extends AppCompatActivity {
+    private InterstitialAd mInterstitialAd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_van_nav_menu);
+        MobileAds.initialize(this,
+                "ca-app-pub-3940256099942544/1033173712");
+
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
+        mInterstitialAd.loadAd(new AdRequest.Builder().build());
     }
 
     public void startAboutActivity(View view){
@@ -33,17 +43,23 @@ public class vanNavMenu extends AppCompatActivity {
     }
 
     public void strartVanScheduleActivity(View view){
+        if (mInterstitialAd.isLoaded()) {
+            mInterstitialAd.show();
+        } else {
+            Log.d("TAG", "The interstitial wasn't loaded yet.");
+        }
         Intent intent = new Intent(this, schedule.class);
         startActivity(intent);
     }
 
-    public void startPirvacyPolicyActivity(View view){
+    public void startPrivacyPolicyActivity(View view){
         Intent intent = new Intent(this, privacypolicy.class);
         startActivity(intent);
     }
 
     public void startTermOfUse(View view){
         Intent intent = new Intent(this, termofuse.class);
+       intent.putExtra("loginStatus", true);
         startActivity(intent);
     }
 }
